7309f68152feef018df28760f1e8c552b49487db
Elon masai <123534505+elonmasai7@users.noreply.github.com>
2024-03-26 11:32:56 +0300

    Update Login_page.php

--- Login_page.php
+++ Login_page.php
@@ -1,48 +1,49 @@
-    <html>  
-    <head>  
-        <title>PHP login system</title>  
-        // insert style.css file inside index.html  
-        <link rel = "stylesheet" type = "text/css" href = "style.css">   
-    </head>  
-    <body>  
-        <div id = "frm">  
-            <h1>Login</h1>  
-            <form name="f1" action = "authentication.php" onsubmit = "return validation()" method = "POST">  
-                <p>  
-                    <label> UserName: </label>  
-                    <input type = "text" id ="user" name  = "user" />  
-                </p>  
-                <p>  
-                    <label> Password: </label>  
-                    <input type = "password" id ="pass" name  = "pass" />  
-                </p>  
-                <p>     
-                    <input type =  "submit" id = "btn" value = "Login" />  
-                </p>  
-            </form>  
-        </div>  
-        // validation for empty field   
-        <script>  
-                function validation()  
-                {  
-                    var id=document.f1.user.value;  
-                    var ps=document.f1.pass.value;  
-                    if(id.length=="" && ps.length=="") {  
-                        alert("User Name and Password fields are empty");  
-                        return false;  
-                    }  
-                    else  
-                    {  
-                        if(id.length=="") {  
-                            alert("User Name is empty");  
-                            return false;  
-                        }   
-                        if (ps.length=="") {  
-                        alert("Password field is empty");  
-                        return false;  
-                        }  
-                    }                             
-                }  
-            </script>  
-    </body>     
-    </html>  
+<?php     
+session_start();    
+if(!isset($_SESSION["sess_user"])){    
+    header("location:login.php");    
+} else {    
+?>    
+<!doctype html>    
+<html>    
+<head>    
+<title>Welcome</title>    
+    <style>     
+        body{    
+                
+    margin-top: 110px;    
+    margin-bottom: 110px;    
+    margin-right: 160px;    
+    margin-left: 90px;    
+    background-color: lightcyan;    
+    color: palevioletred;    
+    font-family: verdana;    
+    font-size: 100%    
+        
+        }    
+            h2 {    
+    color: darkred;    
+    font-family: indigo;    
+    font-size: 100%;    
+}    
+        h1 {    
+    color: darkred;    
+    font-family: indigo;    
+    font-size: 100%;    
+}    
+                
+            
+    </style>    
+</head>    
+<body>    
+    <center><h1>CREATE REGISTRATION AND LOGIN FORM USING PHP IN XAMPP</h1></center>    
+        
+<h2>Welcome, <?=$_SESSION['sess_user'];?>! <a href="logout.php">Logout</a></h2>    
+<p>    
+WE DID IT. SUCCESSFULLY CREATED REGISTRATION AND LOGIN FORM USING PHP IN XAMPP    
+</p>    
+</body>    
+</html>    
+<?php    
+}    
+?>
